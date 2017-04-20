68a6bf09561b69545763ec8a8f329dfa3831d3a1
LdBeth <andpuke@foxmail.com>
2017-04-20 20:50:33 +0800

    removed.

--- index/NMZ.result.normal.pl
+++
@@ -1,5 +0,0 @@
-<dt>${namazu::counter}. <strong><a href="${uri}">${title}</a></strong> (wynik: ${namazu::score})</dt>
-<dd><strong>Autor</strong>: <em>${author}</em></dd>
-<dd><strong>Data</strong>: <em>${date}</em></dd>
-<dd>${summary}</dd>
-<dd><a href="${uri}">${uri}</a> (${size} bajtów)<br><br></dd>
--- index/NMZ.result.short.pl
+++
@@ -1,2 +0,0 @@
-<dt>${namazu::counter}. <strong><a href="${uri}">${title}</a></strong> (wynik: ${namazu::score})</dt>
-<dd><a href="${uri}">${uri}</a> (${size} bajtów)<br><br></dd>
